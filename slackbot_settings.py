38248336174764cd5eef6b601b56213b33564abd
Anna Holmgren <anna.holmgren@wigetmedia.com>
2016-04-11 11:54:40 +0200

    Accidentally committed my API token. It's been changed in production now.

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,4 +1,4 @@
-API_TOKEN = "xoxb-25247001856-v6AC8k3MdCxpqoTk9rgNqdth"
+API_TOKEN = "add your api token here"
 BOT_ICON = "https://avatars.slack-edge.com/2016-03-08/25260263255_135ede00806bb533a8df_48.png"
 PLUGINS = [
     'plugins'
