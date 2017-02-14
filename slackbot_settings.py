3456cbf1922840f32dfb13be171357231554e151
Anna Holmgren <anna.holmgren@wigetmedia.com>
2017-02-14 16:33:57 +0100

    Remove slackbot_settings.py to reduce risk of accidentally committing API token again.

--- slackbot_settings.py
+++
@@ -1,5 +0,0 @@
-API_TOKEN = "xoxb-25247001856-inDKZF9rgFkf5U23C7eFERmT"
-BOT_ICON = "https://avatars.slack-edge.com/2016-03-08/25260263255_135ede00806bb533a8df_48.png"
-PLUGINS = [
-    'plugins'
-]
