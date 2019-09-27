25f158d9a4b8f16d433047f485bf18f21b5d6a1d
Anna Holmgren <anna.holmgren@feelgood.se>
2019-09-27 11:11:05 +0200

    Fix for BAP

--- plugins/restaurants/bap.py
+++ plugins/restaurants/bap.py
@@ -1,11 +1,14 @@
 from functools import lru_cache
 
-from plugins.restaurants.common import add_restaurant, Foodora
+import requests
+from bs4 import BeautifulSoup, Tag
+
+from plugins.restaurants.common import add_restaurant, Item, Kvartersmenyn
 
 
 @add_restaurant
-class Bap(Foodora):
-    url = 'https://www.foodora.se/restaurant/s0es/bap-burgers-pastrami'
+class Bap(Kvartersmenyn):
+    url = 'http://bap.kvartersmenyn.se/'
 
     @staticmethod
     def name():
@@ -17,5 +20,29 @@ class Bap(Foodora):
 
     @lru_cache(32)
     def get(self, year, month, day):
-        return self.parse_page(exclude_headers=['Sides', 'Pastrami', 'Dryck'])
+        result = requests.get(self.url)
+        soup = BeautifulSoup(result.content, 'lxml')
+        menu_items = []
 
+        menu = soup.find('div', {'class': 'meny'})
+        name = ''
+        desc = ''
+        for item in menu.children:
+            if isinstance(item, Tag):
+                t = item.get_text().strip()
+                if len(t) < 2:
+                    continue
+                if t.startswith('TILLVAL'):
+                    break
+                name = t
+                desc = ''
+            else:
+                if name:
+                    d, cost = self.parse_price(item)
+                    desc += d
+                    if cost:
+                        menu_items.append(Item(name=name,
+                                               desc=desc,
+                                               cost=cost))
+
+        return menu_items
